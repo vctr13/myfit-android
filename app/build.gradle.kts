911b7ef6ffd619315ab9fd042d48111de4ca86a5
vctr13 <nikolaich851113@mail.ru>
2026-07-02 15:57:47 +0500

    bump: version 1.1 -> 1.2 (update flow test)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.example.myfit"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
